cd609833b3035d2da8655eaad92120b4147c3846
gmh5225 <2315157@qq.com>
2022-05-13 15:44:28 +0800

    Update

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,7 @@
 static void error(const char* msg, const char* msg1)
 {
     fprintf(stderr, "ERROR: %s%s\n", msg, msg1 ? msg1 : "");
-    exit(1);
+    // exit(1);
 }
 
 void test_sample()
@@ -59,9 +59,55 @@ void test_sample()
     toml_free(conf);
 }
 
+void test_sample2()
+{
+    FILE* fp;
+    char errbuf[200];
+
+    // 1. Read and parse toml file
+    fp = fopen("sample2.toml", "r");
+    if (!fp) {
+        error("cannot open sample2.toml - ", strerror(errno));
+    }
+
+    toml_table_t* conf = toml_parse_file(fp, errbuf, sizeof(errbuf));
+    fclose(fp);
+
+    if (!conf) {
+        error("cannot parse - ", errbuf);
+    }
+
+    // 2. Traverse to a table.
+    toml_table_t* setting = toml_table_in(conf, "setting");
+    if (!setting) {
+        error("missing [setting]", "");
+    }
+
+    // 3. Extract values
+    toml_datum_t enable_1 = toml_bool_in(setting, "enable_1");
+    if (!enable_1.ok) {
+        error("cannot read setting.enable_1", "");
+    }
+
+    printf("enable_1=%d\n", enable_1.u.b);
+
+    toml_datum_t enable_2 = toml_bool_in(setting, "enable_2");
+    if (!enable_2.ok) {
+        error("cannot read setting.enable_2", "");
+    }
+
+    printf("enable_2=%d\n", enable_2.u.b);
+
+
+    // 4. Free memory
+    toml_free(conf);
+}
+
+
 int main()
 {
     test_sample();
+    test_sample2();
     system("pause");
     return 0;
 }
